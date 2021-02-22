24bb4e5e54fa4d23481205a12beac2e41fa17569
esat yıldız <esatyildiz@hotmail.com.tr>
2021-02-23 00:21:44 +0300

    22.02  9. Ders Sonu

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -10,11 +10,14 @@ namespace DataAccess.Concrete.EntityFramework
     public class NorthwindContext:DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=True");
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
     }
 }
